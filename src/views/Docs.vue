f2d133b909eff997d2bcd39a8778394df1ec7767
gtalarico <gtalarico.dev@gmail.com>
2021-09-22 00:57:00 -0700

    Clean up docs

--- src/views/Docs.vue
+++ src/views/Docs.vue
@@ -4,62 +4,77 @@
       <a href="/" class="no-underline"># markdown.is</a>
     </h1>
 
-    <h3>what is this</h3>
+    <h2>what is this</h2>
     <p>
       <strong>markdown.is</strong>
       is a simple website that fetches markdown files from github and renders them as html. The
       initial motivation for this was to be able to view a github profile README.md as a website,
       without taking the visitor to Github. e.g.
-      <a href="/gh/gtalarico">markdown.is/gh/gtalarico</a>
+      <a href="https://markdown.is/gh/gtalarico">markdown.is/gh/gtalarico</a>
       renders
       <a href="https://github.com/gtalarico/gtalarico/README.md">
         github.com/gtalarico/gtalarico/README.md
       </a>
     </p>
 
-    <h3 class="mt-10">what can I use this for?</h3>
+    <h2 class="mt-10">what can I use this for?</h2>
     <ul>
       <li>User your github profile as a website on your bio</li>
       <li>Use any Markdown files on github as a static website</li>
     </ul>
 
-    <h3 class="mt-10">about the url</h3>
-    <h4>defaults</h4>
+    <h2 class="mt-10">about the url</h2>
+    <h3>defaults</h3>
     <p>
       By default, we attempt to render the github profile page of the provided username. For
-      example, `markdown.is/gh/gtalarico` will render
-      <code>github.com/gtalarico/gtalarico/main/README.md</code>
+      example,
+      <a href="https://markdown.is/gh/gtalarico">markdown.is/gh/gtalarico</a>
+      will render
+      <a href="https://github.com/gtalarico/gtalarico/main/README.md">
+        github.com/gtalarico/gtalarico/main/README.md
+      </a>
     </p>
-    <h4>other repositories</h4>
+    <h3>other repositories</h3>
     <p>
       To render files in other repositories, just append it to the url. For example,
-      `markdown.is/gh/gtalarico/pyairtable` will render
-      <code>github.com/gtalarico/pyairtable/main/README.md</code>
+      <a href="https://markdown.is/gh/gtalarico/pyairtable">markdown.is/gh/gtalarico/pyairtable</a>
+      will render
+      <a href="https://github.com/gtalarico/pyairtable/main/README.md">
+        github.com/gtalarico/pyairtable/main/README.md
+      </a>
     </p>
-    <h4>branch</h4>
+    <h3>branch</h3>
     <p>
-      You can also provide other branch names using the query param `branch`.
-      <code>markdown.is/gh/gtalarico?branch=master</code>
+      If you want render a different branch name, you can provide the name using the query param
+      `branch`:
+      <a href="https://markdown.is/gh/gtalarico/pm?branch=master">
+        markdown.is/gh/gtalarico/pm?branch=master
+      </a>
     </p>
 
-    <h3 class="mt-10">custom urls</h3>
-    <h4>alias</h4>
+    <h2 class="mt-10">custom urls</h2>
+    <h3>alias</h3>
     <p>
       You can create custom alias by submitting a PR and adding your alias to
-      <code>src/config.json</code>
+      <a href="https://github.com/gtalarico/markdown.is/edit/main/src/config.json">
+        src/config.json
+      </a>
+
       . The approval of new aliasas is done at the discretion of project maintainers. For example
       adding
       <code>"/gui": "/gh/gtalarico/gtalarico"</code>
       to the config file will allow the profile page of username gtalarico to be available at
       markdown.is/gui
     </p>
-    <h3 class="mt-10">url redirects</h3>
+    <h2 class="mt-10">url redirects</h2>
     <p>
       You can configure your own domain to redirect to your markdown.is page. For example,
-      https://gui.is redirects to https://markdown.is/gui
+      <a href="https://gui.is">https://gui.is</a>
+      redirects to
+      <a href="https://markdown.is/gui">https://markdown.is/gui</a>
     </p>
 
-    <h3 class="mt-10">styling (beta)</h3>
+    <h2 class="mt-10">styling (beta)</h2>
     <p>
       In addition to rendering your markdown, you can also apply a custom style. You can do so by
       adding a css file
